470bc9336efb2e3f5684ca77d3b2cda7e954b608
RedBean <me@hongshn.xyz>
2018-01-10 21:50:48 +0800

    update email example

--- icons-catcher/javascript/email.expample.js
+++ icons-catcher/javascript/email.expample.js
@@ -1,23 +1,26 @@
 var nodeMailer = require('nodemailer');
 
 var transporter = nodeMailer.createTransport({
-  host: '',
+  // service settings
+  service: 'qq',
+  // or
+  host: 'stmp.163.com',
   secureConnection: true,
   port: 465,
   auth: {
-    user: '',
-    pass: ''
+    user: 'example@test.com',
+    pass: 'password'
   }
 });
 
 var mailOps = {
-  from: '',
+  from: 'test <expample@test.com>',
   to: '',
   subject: 'Default',
   text: 'Default text'
 };
 
 exports.sendEmail = function (ops, cb) {
-  var mailOps_ = Object.assign(mailOps, ops);
+  var mailOps_ = Object.assign({}, mailOps, ops);
   transporter.sendMail(mailOps_, function (err, info) { cb(err, info); });
 }
