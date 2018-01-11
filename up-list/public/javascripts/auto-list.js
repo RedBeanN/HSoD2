95971f6d5495c02c15659fb1a9ef4eb911a1efac
RedBean <me@hongshn.xyz>
2018-01-11 16:08:42 +0800

    use axios.js for data update

--- up-list/public/javascripts/auto-list.js
+++ up-list/public/javascripts/auto-list.js
@@ -1,21 +1,63 @@
-(Vue => {
+((Vue, axios) => {
 'use strict';
 
 let app = new Vue({
   el: '#app',
   data: {
-    pools: {'custom': '公主', 'high': '魔女', 'special': '魔法少女'},
-    titles: [],
-    rows: []
+    pools: {'high': '公主', 'custom': '魔女', 'special': '魔法少女'},
+    rows: [],
+    loading: true, error: false,
+    hintText: '加载中...',
+    selectedPool: 'high',
+    sorted: true,
   },
-  computed: {},
-  methods: {
-    select(pool) {
-      console.log(pool);
+  computed: {
+    maxCols() {
+      let maxcols = 0;
+      this.rows.forEach(row => {
+        maxcols = (maxcols < row.data.length) ? row.data.length : maxcols;
+      });
+      return maxcols;
     }
+  },
+  methods: {
+    select(pool = 'high') {
+      // console.log(pool);
+      this.selectedPool = pool;
+      this.loadData(pool);
+    },
+    loadData(pool = 'high') {
+      this.loading = true;
+      this.hintText = '加载中...';
+      axios.get(`/list/auto/${pool}`)
+        .then(res => {
+          app.rows = res.data;
+          app.loading = false;
+          app.error = false;
+        })
+        .catch(err => {
+          app.hintText = '没有找到数据';
+          app.loading = false;
+          app.error = true;
+        });
+    },
+    sortTable() {
+      this.sorted = !this.sorted;
+      let sorted = this.sorted;
+      this.rows.sort((pre, now) => {
+        let preTime = new Date(pre.startTime);
+        let nowTime = new Date(now.startTime);
+        return sorted ? preTime - nowTime : nowTime - preTime;
+      });
+    }
+  },
+  created: function() {
+    // load data here
+    this.select('high');
+    // this.loading = false;
   }
 });
 
 document.getElementById('loading-page').remove();
 document.getElementById('app').style.display = 'block';
-})(Vue);
+})(Vue, axios);
--- up-list/routes/cms.js
+++ up-list/routes/cms.js
@@ -4,7 +4,7 @@ var router = express.Router();
 var fs = require('fs');
 
 // cms
-router.get('/', function(req, res, next) {
+router.get('/:server', function(req, res, next) {
   var titles = JSON.parse(fs.readFileSync('../cms-catcher/statics/titles.json'));
   var arr = [];
   for (var t in titles) {
@@ -18,10 +18,10 @@ router.get('/', function(req, res, next) {
   res.render('cms', {titles: arr, title: '公告备份'});
 });
 
-router.get('/:id', function(req, res, next) {
+router.get('/:server/:id', function(req, res, next) {
   // res.sendfile('public/html/' + req.params.id + '.html');
   var htmlPath = '../cms-catcher/statics/html/';
-  var page = fs.readFileSync(htmlPath + req.params.id + '.html');
+  var page = fs.readFileSync(htmlPath + req.params.server + '/' + req.params.id + '.html');
   res.send(page.toString());
 });
 
