7b24ae092cce3243d5cf183187c23b61cdc73130
RedBean <me@hongshn.xyz>
2018-01-12 13:49:59 +0800

    complete auto-list page with filter and sorter

--- up-list/public/javascripts/auto-list.js
+++ up-list/public/javascripts/auto-list.js
@@ -1,5 +1,5 @@
-((Vue, axios) => {
-'use strict';
+// ((Vue, axios) => {
+// 'use strict';
 
 let app = new Vue({
   el: '#app',
@@ -9,7 +9,11 @@ let app = new Vue({
     loading: true, error: false,
     hintText: '加载中...',
     selectedPool: 'high',
-    sorted: true,
+    sortedFlag: true,
+    searchInput: {
+      isFocus: false,
+      text: ''
+    },
   },
   computed: {
     maxCols() {
@@ -18,6 +22,28 @@ let app = new Vue({
         maxcols = (maxcols < row.data.length) ? row.data.length : maxcols;
       });
       return maxcols;
+    },
+    searchIcon() {
+      if (this.searchInput.isFocus) return '/images/icons/Material/search_black.png';
+      else return '/images/icons/Material/search_white.png';
+    },
+    filterRows() {
+      // if (this.searchInput.text == '') return this.rows;
+      let fr = [];
+      let text = this.searchInput.text;
+      this.rows.forEach(row => {
+        if (row.startTime.indexOf(text) != -1 || row.endTime.indexOf(text) != -1) {
+          fr.push(row);
+        } else {
+          for (let i in row.data) {
+            if (row.data[i].indexOf(text) != -1) {
+              fr.push(row);
+              break;
+            }
+          }
+        }
+      });
+      return fr;
     }
   },
   methods: {
@@ -34,6 +60,8 @@ let app = new Vue({
           app.rows = res.data;
           app.loading = false;
           app.error = false;
+          app.sortedFlag = true;
+          app.sortTable();
         })
         .catch(err => {
           app.hintText = '没有找到数据';
@@ -42,23 +70,34 @@ let app = new Vue({
         });
     },
     sortTable() {
-      this.sorted = !this.sorted;
-      let sorted = this.sorted;
+      this.sortedFlag = !this.sortedFlag;
+      let sortedFlag = this.sortedFlag;
       this.rows.sort((pre, now) => {
         let preTime = new Date(pre.startTime);
         let nowTime = new Date(now.startTime);
-        return sorted ? preTime - nowTime : nowTime - preTime;
+        return sortedFlag ? preTime - nowTime : nowTime - preTime;
       });
+    },
+    focusSearch() {
+      this.searchInput.isFocus = true;
+      document.getElementById('filter-input').classList.remove('hide');
+      document.getElementById('equip-filter').classList.remove('collapse');
+      document.getElementById('filter-input').focus();
+    },
+    blurSearch() {
+      if (this.searchInput.text) return;
+      this.searchInput.isFocus = false;
+      document.getElementById('filter-input').classList.add('hide');
+      document.getElementById('equip-filter').classList.add('collapse');
     }
   },
   created: function() {
     // load data here
     this.select('high');
-    this.sortTable();
     // this.loading = false;
   }
 });
 
 document.getElementById('loading-page').remove();
 document.getElementById('app').style.display = 'block';
-})(Vue, axios);
+// })(Vue, axios);
