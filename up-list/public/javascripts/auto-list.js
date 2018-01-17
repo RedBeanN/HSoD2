1ed0825f4d210beff828e2bb0fb94b781805092e
RedBean <me@hongshn.xyz>
2018-01-17 22:50:27 +0800

    add a dumplicate check for up list

--- up-list/public/javascripts/auto-list.js
+++ up-list/public/javascripts/auto-list.js
@@ -4,16 +4,6 @@
 ((Vue, axios) => {
 'use strict';
 
-function $$(str) {
-  // simply use $$('#id') or $$('.class')
-  if (typeof str != 'string') throw new Error(`Type Error: ${str} is not a string`);
-  switch(str[0]) {
-    case '#': return document.getElementById(str.substring(1));
-    case '.': return document.getElementsByClassName(str.substring(1));
-    default : return document.getElementsByTagName(str);
-  }
-}
-
 let app = new Vue({
   el: '#app',
   data: {
@@ -45,7 +35,8 @@ let app = new Vue({
       let fr = [];
       let text = this.searchInput.text;
       this.rows.forEach(row => {
-        if (row.startTime.indexOf(text) != -1 || row.endTime.indexOf(text) != -1) {
+        if (!row.data.length) {/* ignore empty */}
+        else if (row.startTime.indexOf(text) != -1 || row.endTime.indexOf(text) != -1) {
           fr.push(row);
         } else {
           for (let i in row.data) {
@@ -109,7 +100,6 @@ let app = new Vue({
                 app.hintText = '拉取使魔资料出错！';
                 app.loading = false;
                 app.error = true;
-                console.log(err);
               });
           } else {
             app.rows = res.data;
@@ -126,6 +116,7 @@ let app = new Vue({
         });
     },
     sortTable() {
+      checkDumplicate(this.rows);
       this.sortedFlag = !this.sortedFlag;
       let sortedFlag = this.sortedFlag;
       this.rows.sort((pre, now) => {
@@ -156,4 +147,58 @@ let app = new Vue({
 
 $$('#loading-page').remove();
 $$('#app').style.display = 'block';
+
+/* 一个查重算法
+   递归遍历数组，只关心连续项的重复
+   自动删除重复的部分，然后将重复项的时间调整至应有的范围
+   尾递归调用，不必担心栈溢出
+*/
+function checkDumplicate(arr, now = 1, saved = 0) {
+  if (arr.constructor != Array) throw new Error('Type Error: input data is not an array.');
+  if (now >= arr.length) return;
+  if (arr[now].data.length > arr[saved].data.length) {
+    if (spliceFrom(arr[now].data, arr[saved].data)) {
+      arr[saved].startTime = min(arr[saved].startTime, arr[now].startTime);
+      arr[saved].endTime = max(arr[saved].endTime, arr[now].endTime);
+    } else saved = now;
+  } else {
+    if (spliceFrom(arr[saved].data, arr[now].data)) {
+      arr[now].startTime = min(arr[saved].startTime, arr[now].startTime);
+      arr[now].endTime = max(arr[saved].endTime, arr[now].endTime);
+    } else saved = now;
+  }
+  now++;
+  checkDumplicate(arr, now, saved);
+}
+function spliceFrom(a, b) {
+  if (a.constructor != Array || b.constructor != Array) {
+    throw new Error('Type Error: input datas are not arrays');
+  }
+  let isDumplicated = false;
+  for (let i in a) {
+    for (let j in b) {
+      if (a[i] == b[j]) {
+        a.splice(i, 1);
+        isDumplicated = true;
+      }
+    }
+  }
+  return isDumplicated;
+}
+function min(da, db) { return new Date(da) < new Date(db) ? da : db }
+function max(da, db) { return new Date(da) > new Date(db) ? da : db }
+/* 查重部分结束 */
+
+/* 一个简易的 document.getElementxxx 的包
+   为了避免和 jQuery 冲突使用 $$()
+*/
+function $$(str) {
+  if (typeof str != 'string') throw new Error(`Type Error: ${str} is not a string`);
+  switch(str[0]) {
+    case '#': return document.getElementById(str.substring(1));
+    case '.': return document.getElementsByClassName(str.substring(1));
+    default : return document.getElementsByTagName(str);
+  }
+}
+
 })(Vue, axios);
