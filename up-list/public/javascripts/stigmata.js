2fe173fdfa40fd94edc8361fc34a78b2872160f1
RedBean <me@hongshn.xyz>
2018-03-16 16:02:04 +0800

    stigmata level

--- up-list/public/javascripts/stigmata.js
+++ up-list/public/javascripts/stigmata.js
@@ -3,17 +3,39 @@
 ((Vue) => {
 'use strict';
 
+const cost = [
+  "0", "12", "30", "66", "138", "248", "446", "1115", "1148", "1182",
+  "1217", "1253", "1290", "1328", "1367", "1408", "1450", "1493", "1940",
+  "1998", "2057", "2118", "2160", "2203", "2247", "2291", "2336", "2382",
+  "2429", "2477", "2526", "2576", "2627", "2679", "2732", "2786", "2841",
+  "2883", "2926", "2969", "3013", "3058", "3103", "3149", "3196"
+];
+const levels = {
+  'kiana':   [3, 3, 3, 4, 4, 4, 3, 4, 4, 4, 3, 3, 3, 3],
+  'bronya':  [1, 3, 3, 3, 3, 4, 4, 3, 3, 3, 3, 4, 4, 4],
+  'theresa': [1, 3, 3, 3, 3, 3, 3, 4, 4, 3, 3, 4, 4, 4]
+};
+
 let app = new Vue({
   el: '#app',
-  data: {},
+  data: {
+    character: 'kiana',
+    currentLevel: [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+  },
+  computed: {
+    levels () {
+      return levels[this.character] || levels['kiana'];
+    }
+  },
   methods: {
-    select: function (selected) {
+    select (selected) {
       // change stigmata page
       Array.prototype.forEach.call($$('.option'), op => {
         op.classList.remove('selected');
       });
       $$(`.${selected}`)[0].classList.add('selected');
-      $$('#stigmata').classList = [selected]
+      $$('#stigmata').classList = [selected];
+      this.character = selected;
     }
   }
 });
