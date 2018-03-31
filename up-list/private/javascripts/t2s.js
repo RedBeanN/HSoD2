86d75250dbb50648189ca052210a3191cbf279ba
RedBean <me@hongshn.xyz>
2018-03-31 22:28:26 +0800

    add a t2s api

---
+++ up-list/private/javascripts/t2s.js
@@ -0,0 +1,36 @@
+const http = require('http');
+const qs = require('querystring');
+
+function t2s(text) {
+  return new Promise((resolve, reject) => {
+    let _data = qs.stringify(Object.assign({
+      config: 't2s.json',
+      precise: 0
+    }, { text }));
+    let options = {
+      hostname:'opencc.byvoid.com',
+      port:80,
+      path:'/convert',
+      method:'POST',
+      headers:{
+        'Content-Type':'application/x-www-form-urlencoded; charset=UTF-8',
+        'Content-Length':Buffer.byteLength(_data)
+      }
+    };
+    let req = http.request(options, function(res) {
+        let data = '';
+        res.setEncoding('utf-8');
+        res.on('data', chunk => { data += chunk; });
+        res.on('end',function(){
+          resolve(data);
+        });
+    });
+    req.on('error',function(err){
+      reject(err);
+    });
+    req.write(_data);
+    req.end();
+  });
+}
+
+module.exports = t2s;
--- up-list/routes/index.js
+++ up-list/routes/index.js
@@ -3,6 +3,8 @@ var router = express.Router();
 var path = require('path');
 var fs = require('fs');
 
+const t2s = require('../private/javascripts/t2s');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: '主页 - 搞事学园' });
@@ -18,6 +20,20 @@ router.get('/equip', function (req, res, next) {
 
 router.get('/stigmata', function (req, res, next) {
   res.render('stigmata', { title: '圣痕模拟器 - 搞事学园' });
+});
+
+router.get('/t2s', (req, res, next) => {
+  if (!req.query.text) res.send('?');
+  else {
+    let text = req.query.text;
+    t2s(text)
+      .then(data => {
+        res.send(data);
+      })
+      .catch(err => {
+        res.send(err);
+      });
+  }
 });
 
 module.exports = router;
