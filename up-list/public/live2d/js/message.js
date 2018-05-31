e9c54c03ec1c4948e8d9c2672efc2f4638f374c5
RedBean <me@hongshn.xyz>
2018-05-31 22:09:25 +0800

    formatted

--- up-list/public/live2d/js/message.js
+++ up-list/public/live2d/js/message.js
@@ -1,77 +1,76 @@
 function renderTip(template, context) {
-    var tokenReg = /(\\)?\{([^\{\}\\]+)(\\)?\}/g;
-    return template.replace(tokenReg, function (word, slash1, token, slash2) {
-        if (slash1 || slash2) {
-            return word.replace('\\', '');
-        }
-        var variables = token.replace(/\s/g, '').split('.');
-        var currentObject = context;
-        var i, length, variable;
-        for (i = 0, length = variables.length; i < length; ++i) {
-            variable = variables[i];
-            currentObject = currentObject[variable];
-            if (currentObject === undefined || currentObject === null) return '';
-        }
-        return currentObject;
-    });
+  var tokenReg = /(\\)?\{([^\{\}\\]+)(\\)?\}/g;
+  return template.replace(tokenReg, function (word, slash1, token, slash2) {
+    if (slash1 || slash2) {
+      return word.replace('\\', '');
+    }
+    var variables = token.replace(/\s/g, '').split('.');
+    var currentObject = context;
+    var i, length, variable;
+    for (i = 0, length = variables.length; i < length; ++i) {
+      variable = variables[i];
+      currentObject = currentObject[variable];
+      if (currentObject === undefined || currentObject === null) return '';
+    }
+    return currentObject;
+  });
 }
 
 String.prototype.renderTip = function (context) {
-    return renderTip(this, context);
+  return renderTip(this, context);
 };
 
-function initTips(){
-    $.ajax({
-        cache: true,
-        url: `${message_Path}message.json`,
-        dataType: "json",
-        success: function (result){
-            // console.log(result);
-            window.texts = result.click[0].text[chara]
-            $.each(result.mouseover, function (index, tips){
-                $(tips.selector).mouseover(function (){
-                    var text = tips.text;
-                    if(Array.isArray(tips.text)) text = tips.text[Math.floor(Math.random() * tips.text.length + 1)-1];
-                    text = text.renderTip({text: $(this).text()});
-                    showMessage(text, 3000);
-                });
-            });
-            // $.each(result.click, function (index, tips){
-            //     $(tips.selector).click(function (){
-            //         var text = tips.text[chara];
-            //         if(Array.isArray(tips.text[chara])) text = tips.text[chara][Math.floor(Math.random() * tips.text[chara].length + 1)-1];
-            //         text = text.renderTip({text: $(this).text()});
-            //         showMessage(text, 3000);
-            //     });
-            // });
-        }
-    });
+function initTips() {
+  $.ajax({
+    cache: true,
+    url: `${message_Path}message.json`,
+    dataType: "json",
+    success: function (result) {
+      // console.log(result);
+      window.texts = result.click[0].text[chara]
+      $.each(result.mouseover, function (index, tips) {
+        $(tips.selector).mouseover(function () {
+          var text = tips.text;
+          if (Array.isArray(tips.text)) text = tips.text[Math.floor(Math.random() * tips.text.length + 1) - 1];
+          text = text.renderTip({
+            text: $(this).text()
+          });
+          showMessage(text, 3000);
+        });
+      });
+      // $.each(result.click, function (index, tips){
+      //     $(tips.selector).click(function (){
+      //         var text = tips.text[chara];
+      //         if(Array.isArray(tips.text[chara])) text = tips.text[chara][Math.floor(Math.random() * tips.text[chara].length + 1)-1];
+      //         text = text.renderTip({text: $(this).text()});
+      //         showMessage(text, 3000);
+      //     });
+      // });
+    }
+  });
 }
 initTips();
 
-function showMessage(text, timeout){
-    if(Array.isArray(text)) text = text[Math.floor(Math.random() * text.length + 1)-1];
-    //console.log('showMessage', text);
-    $('.message').stop();
-    $('.message').html(text).fadeTo(200, 1);
-    if (timeout === null) timeout = 5000;
-    hideMessage(timeout);
+function showMessage(text, timeout) {
+  if (Array.isArray(text)) text = text[Math.floor(Math.random() * text.length + 1) - 1];
+  //console.log('showMessage', text);
+  $('.message').stop();
+  $('.message').html(text).fadeTo(200, 1);
+  if (timeout === null) timeout = 5000;
+  hideMessage(timeout);
 }
 
-function hideMessage(timeout){
-    $('.message').stop().css('opacity',1);
-    if (timeout === null) timeout = 5000;
-    $('.message').delay(timeout).fadeTo(200, 0);
+function hideMessage(timeout) {
+  $('.message').stop().css('opacity', 1);
+  if (timeout === null) timeout = 5000;
+  $('.message').delay(timeout).fadeTo(200, 0);
 }
 
-function initLive2d (){
-    $('.hide-button').fadeOut(0).on('click', () => {
-        $('#landlord').css('display', 'none')
-    })
-    $('#landlord').hover(() => {
-        $('.hide-button').fadeIn(600)
-    }, () => {
-        $('.hide-button').fadeOut(600)
-    })
+function initLive2d() {
+  $('#landlord').hover(() => {
+    $('#mute-button').fadeIn(600)
+  }, () => {
+    $('#mute-button').fadeOut(600)
+  })
 }
-initLive2d ();
+initLive2d();
