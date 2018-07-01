e28c1216569fbb33f453911af0b7284045f9d663
RedBean <me@hongshn.xyz>
2018-07-01 17:02:41 +0800

    save raw data

--- world battle watcher/20182/20182.js
+++ world battle watcher/20182/20182.js
@@ -2,6 +2,7 @@ const axios = require('axios');
 const fs = require('fs');
 const path = require('path');
 const json = require('./data.json');
+const raw = require('./raw.json');
 
 const baseUrl = 'http://event.mihoyo.com/activity_api/crossfaction/status';
 
@@ -26,14 +27,28 @@ function getFormatedDate () {
 
 function parseData (data) {
   let top = [], round = [];
-  let {top_data, round_data} = data;
+  let { top_data, round_data } = data;
   if (!top_data || !round_data) console.error('connot find data');
+
   let current = round_data.current_round - 1;
-  top_data.forEach(item => top[item.id - 1] = item.score);
   let roundData = round_data.details[current];
   if (roundData.round - 1 !== current) console.error('error round');
-  roundData.faction_data.forEach(item => round[item.id - 1] = item.score);
-  return {top, round};
+
+  top_data.forEach(item => top[item.id - 1] = item.score);
+  roundData.faction_data.forEach(item => {
+    return round[item.id - 1] = item.score
+  });
+
+  return { top, round };
+}
+
+function saveRawData(time, data) {
+  raw.push({time, data});
+  fs.writeFile(
+    path.resolve(__dirname, './raw.json'),
+    JSON.stringify(raw),
+    console.error
+  );
 }
 
 function watcher () {
@@ -43,8 +58,13 @@ function watcher () {
       data: parseData(data.data)
     };
     json.push(_data);
-    fs.writeFileSync(path.resolve(__dirname,'./data.json'), JSON.stringify(json))
+    fs.writeFile(
+      path.resolve(__dirname,'./data.json'),
+      JSON.stringify(json),
+      console.error
+    )
     // console.log(JSON.stringify(_data, null ,2));
+    saveRawData(_data.time, data.data);
   });
 }
 
