664090defd35da52245fc46d90f0d91b91b5723d
RedBean <me@hongshn.xyz>
2018-07-01 16:32:42 +0800

    move old page

--- up-list/routes/battle.js
+++ up-list/routes/battle.js
@@ -4,12 +4,19 @@ let router = express.Router();
 let path = require('path');
 let fs = require('fs');
 
-router.get('/20181/', (req, res) => {
-  res.render('worldbattle', { title: '阵营战记录 - 搞事学园' });
+router.get('/:time', (req, res) => {
+  res.render(
+    `worldbattle/${req.params.time}`,
+    { title: '阵营战记录 - 搞事学园' }
+  );
 });
 
-router.get('/20181/:data', (req, res) => {
-  fs.readFile(path.join(__dirname, `../../world battle watcher/20181/${req.params.data}.json`), (err, data) => {
+router.get('/:time/:data', (req, res) => {
+  fs.readFile(path.join(
+    __dirname,
+    `../../world battle watcher`,
+    `${req.params.time}/${req.params.data}.json`
+  ), (err, data) => {
     if (err) res.status(404).send(err);
     else res.send(JSON.parse(data));
   });
