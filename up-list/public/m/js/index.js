c479069a75adc8dd9b9d8f891ea54b84601a2df7
RedBean <me@hongshn.xyz>
2018-07-11 16:03:53 +0800

    add a tip for PWA

--- up-list/public/m/js/index.js
+++ up-list/public/m/js/index.js
@@ -6,13 +6,16 @@ const app = new Vue({
     cards: [{
       title: '移动端测试版发布啦 !',
       content: '目前功能还不完善 , 有任何意见建议欢迎发送到 me@hongshn.xyz ~'
+    }, {
+      title: '关于 PWA',
+      content: '使用 https 访问本站 , 添加到主屏幕后就能开始使用 PWA 版本啦 !'
     }],
     pets: [
       { name: '普洛姆特', top: '', round: '' },
-      { name: '冰铃', top: '', round: '' },
-      { name: '书包酱', top: '', round: '' },
-      { name: '茵朵兰', top: '', round: '' },
-      { name: '德哈琳', top: '', round: '' },
+      { name: '冰铃',     top: '', round: '' },
+      { name: '书包酱',   top: '', round: '' },
+      { name: '茵朵兰',   top: '', round: '' },
+      { name: '德哈琳',   top: '', round: '' },
       { name: '奥菲莉亚', top: '', round: '' },
     ],
     drawer: [
@@ -21,11 +24,12 @@ const app = new Vue({
       { url: '/cms/3',             name: '正式服公告备份' },
       { url: '/cms/18',            name: '测试服公告备份' },
       { url: '/buglist',           name: '测试服 BUG 记录' },
-      { url: '/worldbattle/20182', name: '阵营战记录' }
+      { url: '/worldbattle/20182', name: '阵营战记录' },
     ]
   },
   methods: {
     updatePets () {
+      $$('#fw').css('opacity', '0.5');
       const self = this;
       axios.get('/worldbattle/20182/last').then(res => {
         for (let i = 0; i < self.pets.length; i++) {
@@ -34,6 +38,7 @@ const app = new Vue({
             res.data.round[i] : res.data.round[i] === null ?
             '凉凉' : '筹备中';
         }
+        $$('#fw').css('opacity', '1');
       });
     }
   },
