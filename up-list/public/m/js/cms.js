fa022d017d02ffb876f529e944035fcf53a16b98
RedBean <me@hongshn.xyz>
2018-07-19 15:28:15 +0800

    complete cms page for PWA

--- up-list/public/m/js/cms.js
+++ up-list/public/m/js/cms.js
@@ -8,23 +8,52 @@ const app = new Vue({
   data: {
     cards: [],
     searchInput: '',
+    currentServer: 3,
+    isReadingDetail: false,
   },
   computed: {
     filteredCards () {
-      return this.cards;
+      if (this.searchInput === '') return this.cards;
+      return this.cards.filter(item => {
+        for (let i in item) {
+          if (item[i].indexOf(this.searchInput) !== -1) return true;
+        }
+        return false;
+      });
     },
   },
   methods: {
     changeServer (server = 3) {
+      /**
+       * load cms data for server
+       */
       showLoading();
       $$('.servers').removeClass('mdui-tab-active');
       const self = this;
       axios.get('/cms/data/' + server).then(res => {
         self.cards = data2array(res.data);
         $$('#server-' + server).addClass('mdui-tab-active');
+        self.currentServer = server;
+        mdui.mutation();
         hideLoading();
       });
-    }
+    },
+    read (tid) {
+      showLoading();
+      const dialog = new mdui.Dialog($$('#dialog'));
+      axios.get(`/cms/${this.currentServer}/${tid}`, {
+        responseType: 'document'
+      }).then(res => {
+        if (res.data && res.data.body) {
+          $$('#dialog-content').empty().append(res.data.body.children);
+        }
+      }).catch(e => {
+        $$('#dialog-content').empty().append(e.message ? e.message : e);
+      }).then(_ => {
+        dialog.open();
+        hideLoading();
+      });
+    },
   },
   created () {
     this.changeServer();
@@ -40,7 +69,7 @@ function data2array (json) {
     if (key.indexOf('-') === -1) key = '0101-' + key;
     let [date, id] = key.split('-');
     date = date.substr(0, 2) + '-' + date.substr(2);
-    arr.push({ id, date, title: json[key] });
+    arr.push({ id, date, key, title: json[key] });
   }
   return arr.sort((a, b) =>  b.id - a.id);
 }
