2d5bf09699a3a83c0068bba5e67627fd01ca83ca
RedBean <me@hongshn.xyz>
2018-08-30 00:10:46 +0800

    add illustrate page to index

--- up-list/public/m/js/index.js
+++ up-list/public/m/js/index.js
@@ -20,11 +20,12 @@ const app = new Vue({
       exp: 0,
     },
     drawer: [
-      { url: '/list',              name: 'UP 记录' },
-      { url: '/equip',             name: '装备模拟器' },
-      { url: '/cms',               name: '公告记录' },
-      { url: '/buglist',           name: '测试服 BUG 记录' },
-      { url: '/gacha',             name: '扭蛋模拟器' },
+      { url: '/list',       name: 'UP 记录'         },
+      { url: '/equip',      name: '装备模拟器'      },
+      { url: '/cms',        name: '公告记录'        },
+      { url: '/buglist',    name: '测试服 BUG 记录' },
+      { url: '/gacha',      name: '扭蛋模拟器'      },
+      { url: '/illustrate', name: '装备图鉴'        },
     ],
     drawerInst: null,
     swSize: {
