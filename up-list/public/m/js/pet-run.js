d39114a4c36c04975b706abb7fde5f9131f87f0e
RedBean <me@hongshn.xyz>
2019-03-07 16:26:01 +0800

    pet-run history

--- up-list/public/m/js/pet-run.js
+++ up-list/public/m/js/pet-run.js
@@ -1,6 +1,3 @@
-let settings = {
-  scale: 1,
-};
 ((doc, win) => {
   const docEl = doc.documentElement;
   const resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize';
@@ -21,10 +18,29 @@ let settings = {
   win.addEventListener(resizeEvt, recalc, false);
   doc.addEventListener('DOMContentLoaded', recalc, false);
 })(document, window);
+
+const BSearch = (list, cpfn) => {
+  let low = 0;
+  let high = list.length - 1;
+  while (low <= high) {
+    let mid = parseInt((low + high) / 2);
+    let cp = 0;
+    if (typeof cpfn === 'function') cp = cpfn(list[mid]);
+    else cp = cpfn - list[mid];
+    if (cp === 0) return mid;
+    else if (cp > 0) low = mid + 1;
+    else if (cp < 0) high = mid - 1;
+    else return -1;
+  }
+}
+let settings = {
+  scale: 1,
+};
 (() => {
 const assetsHost = 'https://api-1256168079.cos.ap-chengdu.myqcloud.com';
-const $$ = mdui.JQ;
-$$('body').removeClass('mdui-drawer-body-left mdui-appbar-with-tab-larger').addClass('mdui-appbar-with-tab');
+const iconHost = 'https://static.image.mihoyo.com/hsod2_webview/images/broadcast_top/equip_icon/png';
+const $ = mdui.JQ;
+$('body').removeClass('mdui-drawer-body-left mdui-appbar-with-tab-larger').addClass('mdui-appbar-with-tab');
 new Vue({
   el: '#app',
   data: {
@@ -37,6 +53,7 @@ new Vue({
     },
     round: {},
     history: {},
+    pets: [],
     settings,
   },
   methods: {
@@ -57,24 +74,40 @@ new Vue({
       let d = new Date(Number(stamp + '000'));
       return d.toLocaleDateString() + ' ' + d.toTimeString().split(' ')[0];
     },
+    getShortTime (stamp) {
+      let d = new Date(Number(stamp + '000'));
+      return `${d.getDate()}日${d.getHours()}时`
+    },
+    getPetDetail (id) {
+      const pos = BSearch(this.pets, item => id - item.uid);
+      if (pos === undefined || !this.pets[pos]) return null;
+      return this.pets[pos];
+    },
+    getPetIconImg (id) {
+      const detail = this.getPetDetail(id);
+      if (detail !== null && detail.img) {
+        return `${iconHost}/${detail.img}.png`;
+      } else return '/error.png';
+    },
     showLoading () {
-      $$('#progress').css('opacity', '1');
+      $('#progress').css('opacity', '1');
     },
     hideLoading () {
-      $$('#progress').css('opacity', '0');
+      $('#progress').css('opacity', '0');
     },
   },
   async mounted () {
+    const pets = await axios.get('/illustrate/v2/pet');
+    this.pets = pets.data;
     const round = axios.get('https://api.redbean.tech/pet-run/round.json').then(res => {
       this.round = res.data;
     });
     const history = axios.get('https://api.redbean.tech/pet-run/history.json').then(res => {
       this.history = res.data;
     });
-    Promise.all([round, history]).then(() => {
-      this.hideLoading();
-    });
+    Promise.all([round, history])
+    .then(this.hideLoading);
   },
 });
-$$('.hide').removeClass('hide');
+$('.hide').removeClass('hide');
 })();
