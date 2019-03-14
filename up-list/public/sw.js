cfc0838e8308fcbd8ce9126f3401495859d59404
RedBean <me@hongshn.xyz>
2019-03-14 23:57:33 +0800

    sw update

--- up-list/public/sw.js
+++ up-list/public/sw.js
@@ -5,7 +5,7 @@
  * When user visit the site at least twice,
  *   SW caches files for the future visit(s).
  */
-const CACHENAME = 'hsod2-2019.03.03v1';
+const CACHENAME = 'hsod2-2019.03.14v1';
 const urls = [
   /**
    * These files are important and useful for almost all pages.
@@ -76,6 +76,7 @@ const races = [
    */
   'data', 'last', 'auto/',
   'illustrate/v2/',
+  'pet-run/',
 ];
 const excludes = [
   /**
