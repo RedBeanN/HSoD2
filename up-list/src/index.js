a16e17ba4922b94c92949f2a7949c2f0b9390131
RedBeanN <hsn504577431@gmail.com>
2020-02-23 10:48:07 +0800

    remove worldbattle page

--- up-list/src/index.js
+++ up-list/src/index.js
@@ -37,7 +37,7 @@ const app = new Vue({
       { url: '/talent',        name: '圣痕一览',        icon: 'all_out' },
       { url: '/calendar',      name: '崩坏课程表',      icon: 'event_note'},
       // { url: '/roguelike',     name: '伽蓝之末排名',    icon: 'multiline_chart'},
-      { url: '/worldbattle/20201',name: '阵营战记录',   icon: 'multiline_chart'},
+      // { url: '/worldbattle/20201',name: '阵营战记录',   icon: 'multiline_chart'},
       // { url: '/pet-run',       name: '使魔Formula',     icon: 'pets' },
     ],
     drawerInst: null,
