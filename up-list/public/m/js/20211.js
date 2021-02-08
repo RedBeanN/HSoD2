079bc451a35fbe8505fbe8ba26db6b7919e72b64
RedBeanN <me@hongshn.xyz>
2021-02-08 13:17:48 +0800

    world battle 20211

---
+++ up-list/public/m/js/20211.js
@@ -0,0 +1,78 @@
+((Vue, axios) => {
+  const columns = ['时间', "米梅", "伊登", "布伦希尔德", "樱华", "路西法", "拉克西米"];
+  // const columns = ['时间', "暗时计", "幽", "卡奈", "红", "量子小猫", "萨拉"];
+  const parseTime = timeStr => {
+    const [month, day, hour, minute] = timeStr.split(/\s|-|:/g);
+    return `2020/${month}/${day} ${hour}:${minute}:00`;
+  };
+  new Vue({
+    el: '#app',
+    data: {
+      charts: {
+        top1k: {
+          columns,
+          rows: [],
+          title: { text: '分数线' },
+        },
+      },
+      settings: {
+        legendPosition: 'top',
+        loading: false,
+        colors: ['#afeff1', '#454761', '#d1ab4f', '#da5151', '#5755cf', '#e8bc96'],
+      },
+      chartSettings: { scale: [true, true] },
+      zoom: {
+        type: 'slider',
+        start: 0,
+        end: 100,
+      },
+      global: {},
+    },
+    components: { VeLine },
+    created () {
+      this.loadData();
+    },
+    methods: {
+      async loadTop1kData () {
+        const { data } = await axios.get(`https://api-1256168079.cos.ap-chengdu.myqcloud.com/faction/20211/data.json`);
+        const rows = {};
+        for (let key in data) {
+          if (!key.startsWith('faction')) continue;
+          for (let d of data[key].data) {
+            if (!rows[d.time]) rows[d.time] = {};
+            rows[d.time][data[key].factionName] = d.point;
+          }
+        }
+        const rowsArr = [];
+        for (let time in rows) {
+          const obj = {
+            "时间": parseTime(time),
+          };
+          for (let point in rows[time]) {
+            obj[point] = rows[time][point];
+          }
+          rowsArr.push(obj);
+        }
+        rowsArr.sort((a, b) => {
+          return new Date(a['时间']) - new Date(b['时间']) > 0 ? 1 : -1;
+        });
+        this.charts.top1k.rows = rowsArr;
+      },
+      async loadData () {
+        this.settings.loading = true;
+        await this.loadTop1kData();
+        this.settings.loading = false;
+      },
+      format (s) {
+        s = s.toString();
+        if (s.length < 2) return '0' + s;
+        return s;
+      },
+      getDate (d) {
+        const time = new Date(d);
+        return `${time.getMonth() + 1}-${this.format(time.getDate())} ${this.format(time.getHours())}:${this.format(time.getMinutes())}`;
+      },
+    }
+  });
+  
+  })(Vue, axios);
